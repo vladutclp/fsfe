7fda9ca97adb85c7d0118d5077738a421589d5b0
vldclp <vldclp@vldclps-MacBook-Pro.local>
2026-02-21 12:37:16 +0200

    Added sqlite3

--- index-ws.js
+++ index-ws.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const server = require("http").createServer();
-
 const app = express();
 
 app.get("/", function (req, res) {
@@ -8,34 +7,82 @@ app.get("/", function (req, res) {
 });
 
 server.on("request", app);
-server.listen(3000, function () {
-  console.log("server started on port 3000...");
+
+process.on("SIGINT", () => {
+  console.log("sigint");
+
+  wss.clients.forEach((client) => {
+    client.terminate();
+  });
+
+  shutdownDB();
+  process.exit(0);
 });
 
-/** Begin websocket **/
+server.listen(3000, function () {
+  console.log("Listening on 3000");
+});
 
+/** Websocket **/
 const WebSocketServer = require("ws").Server;
 
-const wss = new WebSocketServer({ server });
-console.log("wss", wss);
+const wss = new WebSocketServer({ server: server });
+
 wss.on("connection", function connection(ws) {
-  console.log("from close function from ws");
   const numClients = wss.clients.size;
-  console.log(`Clients connected ${numClients}...`);
+
+  console.log("clients connected: ", numClients);
+
+  // Log number of visitors at current moment
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))`);
 
   wss.broadcast(`Current visitors: ${numClients}`);
 
   if (ws.readyState === ws.OPEN) {
-    ws.send("Welcome to my server");
+    ws.send("welcome!");
   }
 
   ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
     console.log("A client has disconnected");
   });
+
+  ws.on("error", function error() {});
 });
 
 wss.broadcast = function broadcast(data) {
+  console.log("Broadcasting: ", data);
   wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
+/** End Websocket **/
+
+/** Database stuff **/
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(":memory:");
+db.setMaxListeners(20);
+
+db.serialize(() => {
+  db.run(`CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      )`);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  console.log("shutting down DB");
+
+  getCounts();
+  db.close();
+}
+
+/** End Database stuff **/
